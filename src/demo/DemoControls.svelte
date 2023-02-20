248692e58ab0ecb734ceec3508308923b7e5b6e1
Tobias Davis <tobias@davistobias.com>
2023-02-20 16:01:48 -0600

    change: github pages through workflow

--- src/demo/DemoControls.svelte
+++ src/demo/DemoControls.svelte
@@ -19,26 +19,34 @@
 			and automatically open/close if the window is resized. (You can opt out of
 			that behaviour.)
 		</p>
-		<div class="btn-group" role="group" aria-label="Toggle panels">
-			<button type="button" class="btn btn-{leftOpen ? 'primary' : 'secondary'}" on:click={() => dispatch('toggleLeft')}>
-				Toggle Left
-			</button>
-			<button type="button" class="btn btn-{rightOpen ? 'primary' : 'secondary'}" on:click={() => dispatch('toggleRight')}>
-				Toggle Right
-			</button>
+		<div class="form-check fs-5">
+			<input class="form-check-input" type="checkbox" checked={leftOpen} id="left-open" on:click={() => dispatch('toggleLeft')}>
+			<label class="form-check-label" for="left-open">
+				Show Left Panel
+			</label>
+		</div>
+		<div class="form-check fs-5">
+			<input class="form-check-input" type="checkbox" checked={rightOpen} id="right-open" on:click={() => dispatch('toggleRight')}>
+			<label class="form-check-label" for="right-open">
+				Show Right Panel
+			</label>
 		</div>
 		<h4 class="mt-4">Panel animation</h4>
 		<p>
 			The animation of the panels is quick and subtle, here you
 			can slow it down to check for visual errors.
 		</p>
-		<div class="btn-group" role="group" aria-label="Toggle panels">
-			<button type="button" class="btn btn-{panelAnimatesSlowly ? 'secondary' : 'primary'}" on:click={() => { duration = '0.08s'; panelAnimatesSlowly = false }}>
+		<div class="form-check fs-5">
+			<input class="form-check-input" type="radio" name="panelSpeed" id="normal-speed" checked={!panelAnimatesSlowly} on:click={() => { duration = '0.08s'; panelAnimatesSlowly = false }}>
+			<label class="form-check-label" for="normal-speed">
 				Normal Speed
-			</button>
-			<button type="button" class="btn btn-{panelAnimatesSlowly ? 'primary' : 'secondary'}" on:click={() => { duration = '1.5s'; panelAnimatesSlowly = true }}>
+			</label>
+		</div>
+		<div class="form-check fs-5">
+			<input class="form-check-input" type="radio" name="panelSpeed" id="slow-speed" checked={panelAnimatesSlowly} on:click={() => { duration = '1.5s'; panelAnimatesSlowly = true }}>
+			<label class="form-check-label" for="slow-speed">
 				Slow Speed
-			</button>
+			</label>
 		</div>
 	</div>
 </div>
