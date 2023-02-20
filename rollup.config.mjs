7565b34820250c319e11c4d34b83340c64adbca0
Tobias Davis <tobias@davistobias.com>
2023-02-20 15:46:49 -0600

    change: update dev dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import svelte from 'rollup-plugin-svelte'
 import resolve from '@rollup/plugin-node-resolve'
 import css from 'rollup-plugin-css-only'
 import livereload from 'rollup-plugin-livereload'
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
 
 const watch = process.env.ROLLUP_WATCH
 
